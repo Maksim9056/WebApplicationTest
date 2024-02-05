3d1d9fc5fa60eb8f89ced874edf7c8af7e84cbad
Maksim9056 <bobreczovm@inbox.ru>
2024-02-05 12:27:01 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationTest/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        // Другие свойства и методы по необходимости
+    }
+}
